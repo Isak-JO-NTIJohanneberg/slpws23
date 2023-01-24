556ea9e4e0a7068e833bcdaac5bfb05493f75442
Isak-JO-NTIJohanneberg <isak.jonssonohstrom@elev.ga.ntig.se>
2023-01-24 11:30:04 +0100

    gjort stor del av min databas struktur och skapat tabeller med kolumner, dessutom har jag skapat restful-route-sidor.

--- app.rb
+++ app.rb
@@ -9,6 +9,22 @@ get('/') do
 end
 
 
-get('/annonser') do
-    slim(:index)
+get('/annonser/') do
+    db = SQLite3::Database.new("db/AD_DATA.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT rubrik FROM Annonser").first
+    slim(:"annonser/index", locals:{result:result})
 end
+
+get('/annonser/:id') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/AD_DATA.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM Annonser WHERE id = ?",id).first
+    slim(:"annonser/show",locals:{result:result})
+end
+
+get('/sparade/') do
+    slim(:"sparade/index")
+end
+
